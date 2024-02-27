7da2b2d53c6e6b360121d71ec188f1ee4b45e414
Julian094 <jc94.jm@gmail.com>
2024-02-26 19:37:38 -0500

    interaccion de teleport creada || activación de animaciones por medio de wigtet creada || recursos 3D añadidos || ambientación añadida al nivel

---
+++ Source/PolygonTest/Private/Interactions/PT_TeleportingObject.cpp
@@ -0,0 +1,111 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactions/PT_TeleportingObject.h"
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+APT_TeleportingObject::APT_TeleportingObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpectatingBlendTime = 2.0f;
+	ParameterNameEffect = "Appearance";
+
+	CustomRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("CustomRootComponent"));
+	RootComponent = CustomRootComponent;
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(RootComponent);
+	
+	MeshTeleportComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponentTeleport"));
+	MeshTeleportComponent->SetupAttachment(RootComponent);
+
+	SpectatingCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SpectatingCameraComponent"));
+	SpectatingCameraComponent->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void APT_TeleportingObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialLcoation = GetActorLocation();
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &APT_TeleportingObject::StartInteraction);
+
+	ObjectMaterial = MeshTeleportComponent->CreateAndSetMaterialInstanceDynamicFromMaterial(0, MeshTeleportComponent->GetMaterial(0));	
+}
+
+void APT_TeleportingObject::Teleport(AActor* ObjectivePortal)
+{
+	if (IsValid(ObjectivePortal))
+	{
+		//SetActorLocation(ObjectivePortal->GetActorLocation());
+		MeshTeleportComponent->SetWorldLocation(ObjectivePortal->GetActorLocation());
+		BP_SetInitialVisualEffect();
+	}
+}
+
+void APT_TeleportingObject::PutObjectOnInitialLocation()
+{
+	MeshTeleportComponent->SetWorldLocation(InitialLcoation);
+}
+
+void APT_TeleportingObject::StartInteraction(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ACharacter* RefCharacter = Cast<ACharacter>(OtherActor);
+	if (IsValid(RefCharacter))
+	{
+		AController* RefController = RefCharacter->GetController();
+		if (IsValid(RefController))
+		{
+			APlayerController* PlayerController = Cast<APlayerController>(RefController);
+			if (IsValid(PlayerController))
+			{
+				BP_StarInteraction();
+				PlayerController->SetViewTargetWithBlend(SpectatingCameraComponent->GetOwner(), SpectatingBlendTime, VTBlend_Cubic);
+				RefCharacter->GetCharacterMovement()->SetMovementMode(MOVE_None);
+			}
+		}
+	}
+}
+
+void APT_TeleportingObject::EndInteraction()
+{
+	ACharacter* RefCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	ACharacter* PlayerCharacter = Cast<ACharacter>(RefCharacter);
+	if (IsValid(PlayerCharacter))
+	{
+		AController* RefController = PlayerCharacter->GetController();
+		if (IsValid(RefController))
+		{
+			APlayerController* PlayerController = Cast<APlayerController>(RefController);
+			if (IsValid(PlayerController))
+			{
+				PlayerController->SetViewTargetWithBlend(PlayerCharacter, SpectatingBlendTime, VTBlend_Cubic);
+				PlayerCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+				PutObjectOnInitialLocation();
+				BP_SetInitialVisualEffect();
+			}
+		}
+	}
+}
+
+void APT_TeleportingObject::VisualEffect(float Value)
+{
+	ObjectMaterial->SetScalarParameterValue(ParameterNameEffect, Value);
+}
+
+
+
+
+
+
---
+++ Source/PolygonTest/Public/Interactions/PT_TeleportingObject.h
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PT_TeleportingObject.generated.h"
+
+class UCameraComponent;
+class UBoxComponent;
+class UStaticMeshComponent;
+class USceneComponent;
+class UMaterialInstanceDynamic;
+
+UCLASS()
+class POLYGONTEST_API APT_TeleportingObject : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	USceneComponent* CustomRootComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UBoxComponent* BoxComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UStaticMeshComponent* MeshTeleportComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UCameraComponent* SpectatingCameraComponent;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Teleport|Effects")
+	FName ParameterNameEffect;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Teleport")
+	float SpectatingBlendTime;
+
+	UPROPERTY(BlueprintReadOnly, Category="Teleport")
+	FVector InitialLcoation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Teleport")
+	TMap<AActor*, FName> TargetsPointsNames;
+
+	UMaterialInstanceDynamic* ObjectMaterial;
+	
+public:	
+	// Sets default values for this actor's properties
+	APT_TeleportingObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category="Teleport")
+	void Teleport(AActor* ObjectivePortal);
+
+	UFUNCTION(BlueprintCallable, Category="Teleport")
+	void PutObjectOnInitialLocation();
+
+	UFUNCTION()
+	void StartInteraction(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION(BlueprintCallable, Category="Teleport")
+	void EndInteraction();
+
+	UFUNCTION(BlueprintCallable, Category="Telepor|Effects")
+	void VisualEffect(float Value);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_StarInteraction();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_SetInitialVisualEffect();
+
+};
